9a7f863bb89334c9533ab39d60517f88241094eb
Igol4 <94765726+Igol4@users.noreply.github.com>
2021-11-21 18:19:14 +0100

    Update autoclicker.cpp

--- autoclicker.cpp
+++ autoclicker.cpp
@@ -2,15 +2,20 @@
 #include<Windows.h>
 
 using namespace std;
+int a;
+char b;
 void menu()
 {
-	cout << "Press 'A' to enable and 'B' to disable autoclicker\n"; //you can change letters by simply replacing them in if and else if block//
+	cout << "Input difference between each click(miliseconds)\n";
+	cin >> a;
+	cout << "Press 'A' to enable and 'B' to disable autoclicker\n"; //you can change letters by simply replacing them in if and else if block
 }
 void clicker()
 {
 	bool click = false;
 	while (true)
 	{
+	
 		if(GetAsyncKeyState('A'))
 		{
 			click = true;
@@ -18,17 +23,32 @@ void clicker()
 		else if(GetAsyncKeyState('B'))
 		{
 			click = false;
+			cout << "Do you want to restart the program or continue with current settings [c/r]\n?";
+			cin >> b;
+			if(b == 'r')
+			{   
+				menu();
+			}
+			else if(b == 'c')
+			{
+				cout << "Press 'A' to enable and 'B' to disable autoclicker\n";
+			}
+			else
+			{
+				cout << "You had a typo, type c to continue or r to restart. Program will close now.\n";
+				Sleep(4000);
+				exit(0);
+			}
 		}
 		if(click == true)
 		{
 			mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,0);
 			mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);
-			Sleep(10); //time between each click//
+			Sleep(a); //time between each click
 			
 		}
 	}
 }
-
 int main()
 {
   menu();
